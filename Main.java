99e41a3938ba66cbbf91a6745b6917c04b085f0e
michaelz126 <1zhang.michael@gmail.com>
2024-10-09 13:24:47 +0000

    finished

--- Main.java
+++ Main.java
@@ -4,10 +4,27 @@ public class Main
     {
         String info;
         Scoreboard game = new Scoreboard("Red", "Blue");
-        game.recordPlay(1);
+        //game.recordPlay(0);
+        //game.recordPlay(3);
         info = game.getScore();
-        System.out.println(info);
         
+        game.recordPlay(1);
+        
+        game.recordPlay(0);
+        game.recordPlay(3);
+        game.recordPlay(1);
+        game.recordPlay(0);
+        game.recordPlay(0);
+        game.recordPlay(4);
+        game.recordPlay(0);
+        info = game.getScore();
+
+        
+        System.out.println(info);
+        Scoreboard match = new Scoreboard("Lions", "Tigers");
+        info = match.getScore();
+        System.out.println(info);
+    
     }
 
 }
